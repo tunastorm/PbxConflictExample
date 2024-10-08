573ef1596609deb047d636ffbf3694b0e4cd5763
tunastorm <ycw921@naver.com>
2024-10-08 11:13:53 +0900

    Initial Commit

---
+++ PbxConflictExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PbxConflictExample
+//
+//  Created by 유철원 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PbxConflictExample/PbxConflictExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PbxConflictExampleApp.swift
+//  PbxConflictExample
+//
+//  Created by 유철원 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PbxConflictExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
